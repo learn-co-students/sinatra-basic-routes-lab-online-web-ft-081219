5f27d991039cb8710a22fa4374cab5203d001a28
corbinarnett <corbin.c.arnett@gmail.com>
2019-10-01 00:30:13 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Corbin."
+  end
+
+  get '/hometown' do
+    "My hometown is Portland, OR."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'no scrubs'."
+  end
 end
