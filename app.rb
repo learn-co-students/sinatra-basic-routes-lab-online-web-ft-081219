2ffe10435974d44153ab339c57895a126bcd87b8
Denali Balser <denali.balser167@gmail.com>
2019-10-02 20:49:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name'  do
+    "My name is Denali"
+  end
+
+  get '/hometown' do
+    "My hometown is Pomfret"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is You are Losing Me"
+  end
 end
